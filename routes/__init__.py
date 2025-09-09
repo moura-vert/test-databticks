31f98cc697f3ce39773a3e3f1b5a4b63feccfc1e
Carlos Moura <moura@vert-capital.com>
2025-09-09 13:46:42 -0300

    hello world

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter
 
 # Import routers from versioned packages
+
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from .v1 import router as v1_router
 
-# Create a router for the API
+
 api_router = APIRouter()
+
+# Rota default
+@api_router.get("/")
+async def root():
+	return JSONResponse({"message": "Hello, World!"})
 
 # Include versioned routers - prefix must have /api for Databricks Apps token-based auth
 api_router.include_router(v1_router, prefix="/api/v1")
